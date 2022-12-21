87daa8aea7044db34699081e582259902faef8cf
Abhijeet Satpute <abhi111sat@gmail.com>
2022-12-21 18:08:43 +0530

    serving swagger api docs at /api-docs

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 dotenv.config();
 
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
