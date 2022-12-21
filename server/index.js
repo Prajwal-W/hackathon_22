f41dad206cd1d4d6cd120cc9665d821735664df5
Abhijeet Satpute <abhi111sat@gmail.com>
2022-12-21 18:24:11 +0530

    db connect

--- server/index.js
+++ server/index.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const express = require("express");
+const dotenv = require("dotenv");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 const app = express();
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
+app.get("/", (req, res, next) => {
+  res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
-    console.log('Litening on port ' + process.env.PORT);
-});
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((result) => {
+    console.log("🎉 Database connected.");
+    app.listen(process.env.PORT, function () {
+      console.log("✨ App listening on port", this.address().port);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
