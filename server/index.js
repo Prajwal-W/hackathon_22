10c9c59b7fdfadf817e2d76deaa6e8f6d3e9e76c
Abhijeet Satpute <abhi111sat@gmail.com>
2022-12-22 20:41:29 +0530

    added cors middleware

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   console.log("HTTP Method: " + req.method + " , URL: " + req.url);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
