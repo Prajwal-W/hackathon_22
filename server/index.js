82ad78f37961f86ba339273e2d976edbf3d85b5d
Abhijeet Satpute <abhijeetsatpute98@gmail.com>
2022-12-29 14:17:02 +0000

    Removed CORS middleware code

--- server/index.js
+++ server/index.js
@@ -17,9 +17,9 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   console.log("HTTP Method: " + req.method + " , URL: " + req.url);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
